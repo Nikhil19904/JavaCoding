3ccd1a653c2066d440a1e131745fdf49198ef3ed
Nikhil <110816592+Nikhil19904@users.noreply.github.com>
2023-10-15 15:54:40 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,14 @@
+//basic and creation of an array.
+
+public class array1 {
+public static void main(String[] args) {
+  /*   int marks[]=new int[10];
+    marks[0]=78;
+    marks[1]=89;
+    marks[2]=63;
+    marks[3]=98;
+    marks[4]=56;*/
+    int marks[]={56,87,43,90};
+    System.out.println(marks[3]);
+}
+}
